1000253a7bc17da48358fa47a5a5e7446bcc2dcb
Steven Barkley <steven.barkley@outlook.com>
2022-11-04 21:20:37 -0400

    Created php local repo

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+        // Set a variable with your starting number
+        $startingNum = 10;
+
+        // While $startingNum is less than or equal to 20
+        while ($startingNum <= 20) {
+            // execute code
+        
+        echo $startingNum . "<br>";
+            // incremeent the value by 1
+        $startingNum++;
+        }
+?>
+
+
+<?php
+// PHP Functions
+
+sort()
+rsort()
+strtolower()
+sha1()
+
+function functionName() {
+    // execute code
+}
+
+?>
